ab25143bd125ece76dde5d55a30e955453724e3d
red-tt-bear <red.tt.bear@gmail.com>
2021-07-06 20:20:55 -0400

    Added home page

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+interface HomePageProps {
+  /**
+   * The name of the person this resume website belongs to
+   */
+  name: string;
+  missionStatement?: string;
+  style?: React.CSSProperties;
+  backgroundImageURL?: string;
+  backgroundColor?: string;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const HomePage = ({
+    name,
+    style = { height: '100vh' },
+    backgroundImageURL,
+    backgroundColor,
+    missionStatement,
+}: HomePageProps) => {
+  const propStyles: React.CSSProperties = {};
+  if (backgroundImageURL) {
+    propStyles.backgroundImage = `URL(${backgroundImageURL})`;
+  }
+
+  if (backgroundColor) {
+    propStyles.backgroundColor = backgroundColor;
+  }
+
+  return (
+    <div
+      className="zrt-home-root"
+      style={{
+        ...style,
+        ...propStyles,
+        display: 'flex',
+      }}
+    >
+      <div
+        className="zrt-home-content"
+        style={{
+          margin: 'auto',
+          textAlign: 'center',
+          maxWidth: '75%'
+        }}
+      >
+        <h1 className="zrt-name">
+          {name}
+        </h1>
+        { missionStatement &&
+        <h4 className="zrt-mission-statement">
+          {missionStatement}
+        </h4>}
+      </div>
+    </div>
+  );
+};
---
+++ src/stories/HomePage.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { HomePage } from '../components/HomePage';
+
+export default {
+  title: 'Individual Pages/Home',
+  component: HomePage,
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+      defaultValue: '#EEEEEE'
+    },
+    missionStatement: { defaultValue: 'Default Mission Statement' },
+    name: { defaultValue: 'Default Name'},
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof HomePage>;
+
+const Template: ComponentStory<typeof HomePage> = (args) => <HomePage {...args} />;
+
+export const DefaultStory = Template.bind({});
+DefaultStory.args = {};
+
+export const LongNameShortMission = Template.bind({});
+LongNameShortMission.args = {
+  name: 'abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz',
+  missionStatement: 'short'
+};
+
+export const ShortNameLongMission = Template.bind({});
+ShortNameLongMission.args = {
+  name: 'Short Name',
+  missionStatement: 'abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz abdefghijklmnopqrstuvwxyz'
+};
