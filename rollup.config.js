c1623f30e5fb39d79ee2694600710496b43cecb7
red-tt-bear <red.tt.bear@gmail.com>
2021-07-07 22:24:57 -0400

    Cleaned up code now that rollup is working

--- rollup.config.js
+++ rollup.config.js
@@ -6,44 +6,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import packageJSON from './package.json';
 
-// export default [
-//     {
-//         input: './src/index.ts',
-//         output: [
-//             {
-//                 file: packageJSON.main,
-//                 format: 'cjs',
-//                 exports: 'named',
-//                 sourcemap: true,
-//             },
-//             // {
-//             //     file: packageJSON.module,
-//             //     format: 'esm',
-//             //     exports: 'named',
-//             //     sourcemap: true,
-//             // },
-//             // {
-//             //     file: 'dist/index.esm.js',
-//             //     format: 'esm',
-//             //     exports: 'named',
-//             //     sourcemap: true,
-//             // },
-//         ],
-//         plugins: [
-//             // This solves esinterop issues (as of right now lodash and moment were giving issues with default import)
-//             commonjs(),
-//             typescript(),
-//             babel({
-//                 exclude: 'node_modules/**',
-//                 presets: ['@babel/preset-react']
-//             }),
-//             external(),
-//             resolve(),
-//             // minify output
-//             terser(),
-//         ]
-//     }
-// ]
 export default [
     {
         input: './src/index.ts',
@@ -60,27 +22,25 @@ export default [
             //     exports: 'named',
             //     sourcemap: true,
             // },
-            // {
-            //     file: 'dist/index.esm.js',
-            //     format: 'esm',
-            //     exports: 'named',
-            //     sourcemap: true,
-            // },
         ],
         plugins: [
-            external(),
-            resolve(),
             // This solves esinterop issues (as of right now lodash and moment were giving issues with default import)
             commonjs(),
             typescript({
+                // This should be default, however this fixed an issue in which declaration files were not generated (as well as source maps)
+                // https://github.com/rollup/plugins/issues/61#issuecomment-845838534
                 tsconfig: './tsconfig.json',
+                // If not defined it duplicates the output dir, in this case defs would be in dist/dist
+                declarationDir: '.',
             }),
             babel({
                 exclude: 'node_modules/**',
                 presets: ['@babel/preset-react']
             }),
+            external(),
+            resolve(),
             // minify output
-            // terser(),
+            terser(),
         ]
     }
 ]
