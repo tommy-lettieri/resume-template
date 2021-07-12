c5aae9a1e338ad65095ec80979745df448c7b59f
red-tt-bear <red.tt.bear@gmail.com>
2021-07-11 21:28:39 -0400

    Switched employment to use the generic components

--- src/components/Website.tsx
+++ src/components/Website.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { EducationAPIWrapper } from './context-wrappers/EducationContext';
-import { EmploymentAPIWrapper } from './context-wrappers/EmploymentPageContextWrapper';
 import { HomeAPIWrapper } from './context-wrappers/HomePageContextWrapper';
 import { GenericAPIWrapper } from './context-wrappers/GenericPageContextWrapper';
-import { ProjectsContext, SkillsContext, AchievementsContext } from '../contexts';
+import { ProjectsContext, SkillsContext, AchievementsContext, EmploymentContext } from '../contexts';
 
 export interface WebsiteProps {
     style?: React.CSSProperties;
@@ -24,7 +23,7 @@ export const Website = ({
             padding: '20px',
         }} className="zrt-website">
             <GenericAPIWrapper DataContext={SkillsContext} name='skills' />
-            <EmploymentAPIWrapper />
+            <GenericAPIWrapper DataContext={EmploymentContext} name='employment' />
             <EducationAPIWrapper />
             <GenericAPIWrapper DataContext={AchievementsContext} name='achievements' />
             <GenericAPIWrapper DataContext={ProjectsContext} name='projects' />
--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -6,3 +6,4 @@ type DataContextType = Omit<GenericPageProps, 'pageName'>;
 export const ProjectsContext = createContext<UseStateType<DataContextType | null | undefined> | null>(null);
 export const SkillsContext = createContext<UseStateType<DataContextType | null | undefined> | null>(null);
 export const AchievementsContext = createContext<UseStateType<DataContextType | null | undefined> | null>(null);
+export const EmploymentContext = createContext<UseStateType<DataContextType | null | undefined> | null>(null);
