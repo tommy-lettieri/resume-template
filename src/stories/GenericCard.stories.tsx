8bfbfb8e17df669f40cd3058981d6bb80c88dfb7
red-tt-bear <red.tt.bear@gmail.com>
2021-07-15 22:11:18 -0400

    Updated test data

--- src/stories/GenericCard.stories.tsx
+++ src/stories/GenericCard.stories.tsx
@@ -30,7 +30,7 @@ export default {
                 startDate: new Date(),
                 endDate: null,
                 website: window.location.href,
-                logoURL: 'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v286batch2-aew-company-logo-17_4.jpg?w=1200&h=1200&dpr=1&fit=clip&crop=default&fm=jpg&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=e93e1706e26a78ff1238b3ca63b10bdf',
+                logoURL: '/placeholder.png',
             },
         },
         logoWidth: { defaultValue: 100 },
