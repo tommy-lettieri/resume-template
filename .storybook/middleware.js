2809c3b1cc207c2f5c1b255d1de502eb68204e1b
red-tt-bear <red.tt.bear@gmail.com>
2021-07-23 11:49:15 -0400

    Added gh-page deployment of storybook
    Fixed build storybook for public assets
    Put comment as to why I don't do that for development

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -1,3 +1,8 @@
+/**
+ * Could also use -s option or --static-dir (https://storybook.js.org/docs/react/api/cli-options)
+ * However that can't be set as an environment variable nor can you make a fallback
+ * Lastly this is more realistic to match consumers allowing them to use sub routes as their public url
+ */
 const express = require('express');
 const path = require('path');
 
